81f941321dd038a2c0793d45269642fbfcd39dc5
Vinícius Müller <vinigm.nho@gmail.com>
2021-09-13 21:22:21 -0300

    Build functions return pointers and add reverse

--- data-structures/List/dynamic_array.c
+++ data-structures/List/dynamic_array.c
@@ -8,42 +8,56 @@ typedef struct {
 } DynamicArray;
 
 // Helper functions
-inline void free_array(DynamicArray *dyn);
-void _dynamic_array_realloc(DynamicArray *dyn);
-DynamicArray init_array();
+DynamicArray *init_array();                       // O(1)
+DynamicArray *from_array(int *array, int length); // O(n)
+void free_array(DynamicArray *dyn);               // O(1)
+void _dynamic_array_realloc(DynamicArray *dyn);   // O(1)
 
 // List ADT API
 void push(DynamicArray *dyn, int data);            // O(1)
 void insert(DynamicArray *dyn, int data, int idx); // O(n)
 void delete (DynamicArray *dyn, int idx);          // O(n)
-void show(DynamicArray *dyn);                      // O(n)
 inline int at(DynamicArray *dyn, int idx);         // O(1)
 
+// Extras
+void show(DynamicArray *dyn);    // O(n)
+void reverse(DynamicArray *dyn); // O(n)
+
 int main() {
-  DynamicArray dyn = init_array();
+  DynamicArray *dyn = init_array();
 
   for (int i = 0; i <= 10; i++) {
-    push(&dyn, i);
+    push(dyn, i);
   }
 
+  // Insertion/Deletion
   printf("Before deleting at position 2\n");
-  show(&dyn);
-  delete (&dyn, 2);
+  show(dyn);
+  delete (dyn, 2);
   printf("After deleting at position 2\n");
-  show(&dyn);
-
+  show(dyn);
   printf("Before inserting 777 at position 2\n");
-  show(&dyn);
-  insert(&dyn, 777, 2);
+  show(dyn);
+  insert(dyn, 777, 2);
   printf("After inserting 777 at position 2\n");
-  show(&dyn);
+  show(dyn);
 
-  free_array(&dyn);
+  // Reversing
+  puts("after reversing array");
+  reverse(dyn);
+  show(dyn);
+
+  free_array(dyn);
 }
 
-DynamicArray init_array() {
-  return (DynamicArray){
-      .length = 0, .capacity = 32, .data = malloc(32 * sizeof(int))};
+DynamicArray *init_array() {
+  DynamicArray *dyn = malloc(sizeof(DynamicArray));
+  dyn->length = 0;
+  dyn->capacity = 32;
+  dyn->data = malloc(32 * sizeof(int));
+  /* dyn = (DynamicArray*){ */
+  /*     .length = 0, .capacity = 32, .data = malloc(32 * sizeof(int))}; */
+  return dyn;
 }
 
 void _dynamic_array_realloc(DynamicArray *dyn) {
@@ -83,6 +97,17 @@ void delete (DynamicArray *dyn, int idx) {
   dyn->length--;
 }
 
+void reverse(DynamicArray *dyn) {
+  int *new_buffer = malloc(sizeof(int) * dyn->capacity);
+
+  for (int i = dyn->length; i >= 0; i--) {
+    new_buffer[dyn->length - i - 1] = at(dyn, i);
+  }
+
+  free(dyn->data);
+  dyn->data = new_buffer;
+}
+
 void show(DynamicArray *dyn) {
   // Iterate through the elements and print them
   printf("[");
@@ -92,6 +117,22 @@ void show(DynamicArray *dyn) {
   printf("%d]\n", dyn->data[dyn->length - 1]);
 }
 
-inline void free_array(DynamicArray *dyn) { free(dyn->data); }
+DynamicArray *from_array(int *array, int length) {
+  DynamicArray *dyn = init_array();
+
+  for (int i = 0; i < length; i++) {
+    push(dyn, array[i]);
+  }
+
+  return dyn;
+}
+
+void free_array(DynamicArray *dyn) {
+  free(dyn->data);
+  dyn->data = NULL;
+
+  free(dyn);
+  dyn = NULL;
+}
 
 inline int at(DynamicArray *dyn, int idx) { return dyn->data[idx]; }
